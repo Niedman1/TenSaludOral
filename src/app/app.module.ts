19e5dfb191ef5b8655eb9da772eeb3873201d1fa
santiagoae <santiago_avendano82162@elpoli.edu.co>
2022-09-07 15:33:52 -0500

    firstcommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { CarruselComponent } from './components/carrusel/carrusel.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { EspecialistasComponent } from './components/especialistas/especialistas.component';
+import { AlianzasComponent } from './components/alianzas/alianzas.component';
+import { ComoLlegarComponent } from './components/como-llegar/como-llegar.component';
+import { RedesComponent } from './components/redes/redes.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    CarruselComponent,
+    ServiciosComponent,
+    EspecialistasComponent,
+    AlianzasComponent,
+    ComoLlegarComponent,
+    RedesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
