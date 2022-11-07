db6f7b99d64fa39a6543f9cdc8502d5ebcd32299
Yessica Alejandra Piedrahita <Yessica_piedrahita82181@elpoli.edu.co>
2022-11-07 13:40:42 -0500

    se termina crud

--- src/app/pages/registrar/registrar.component.ts
+++ src/app/pages/registrar/registrar.component.ts
@@ -11,6 +11,7 @@ import { UsuarioService } from './../../servicios/usuario.service';
 export class RegistrarComponent implements OnInit {
 
   usuarioForm: FormGroup;
+  usuario: any;
 
   constructor(public fb: FormBuilder, public Usuario: UsuarioService) {}
 
@@ -19,23 +20,53 @@ export class RegistrarComponent implements OnInit {
   ngOnInit(): void {
 
     this.usuarioForm = this.fb.group({
+      id: [''],
       nombre: ['', Validators.required],
       apellidos: ['', Validators.required],
       correo: ['', Validators.required],
       contra: ['', Validators.required],
       cedula: ['', Validators.required],
-    })
+    });
 
+    this.Usuario.getAllUsuarios().subscribe(Response => {
+      this.usuario= Response
+    },
+    error => {console.error(error)}
+    )
 
   }
 
   guardar(): void {
 this.Usuario.saveUsuario(this.usuarioForm.value).subscribe(Response => {
-
+  this.usuarioForm.reset();
+  this.usuario=this.usuario.filter(usuario=>Response.id!=usuario.id)
+  this.usuario.push(Response);
 },
 error => {console.error(error)}
-)}
-    
-
+)}   
  
+eliminar (usuario){
+  this.Usuario.deleteUsuario( usuario.id ).subscribe(Response => {
+    //console.log(Response)
+    if(Response===true){
+      this.usuario.pop(usuario);
+
+    }
+
+  },
+  error => {console.error(error)}
+  )}   
+
+
+  editar(usuario){
+    this.usuarioForm.setValue ({
+      id: usuario.id,
+      nombre: usuario.nombre ,
+      apellidos: usuario.apellidos ,
+      correo: usuario.correo ,
+      contra: usuario.contra ,
+      cedula: usuario.cedula ,
+    });
+  }
+
 }
